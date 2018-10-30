edc05d8db504c729f88cfcdc048837455b8d757f
shaiktajuddin <nimmufarju058>
2018-10-30 11:06:38 +0530

    update component

---
+++ src/components/update.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class update extends Component {
+  render() {
+    return (
+      <div>
+        <h3>hai hellio</h3>
+      </div>
+    )
+  }
+}
